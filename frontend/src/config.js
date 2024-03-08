e77749ae10d92bbfaf171c773a4c4bdb91299d70
Your Name <shaymrosner@gmail.com>
2024-03-07 18:15:04 -1000

    clean frontend structure, baseurl and add bootstrap

---
+++ frontend/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  development: {
+    baseURL: "http://127.0.0.1:5000/",
+  },
+  production: {
+    // Todo: update with production base url
+    baseURL: "https://your-production-url.com/",
+  },
+};
+
+export default config;
